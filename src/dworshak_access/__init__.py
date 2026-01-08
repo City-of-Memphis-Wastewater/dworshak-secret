99c7410eb45952146116890783f7c19f9087601c
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-01-08 13:00:40 -0600

    extra s, dworshak_accesss -> dworshak_access, in __init__.py

--- src/dworshak_access/__init__.py
+++ src/dworshak_access/__init__.py
@@ -1,6 +1,6 @@
 # src/dowrshak_access/__init__.py
 
-from dworshak_accesss.vault import get_secret, check_vault
+from dworshak_access.vault import get_secret, check_vault
 
 __all__ = [
     "get_secret",
