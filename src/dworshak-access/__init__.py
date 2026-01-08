0ee31460121e39a485a21e4c375ff94b532441b4
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-01-07 20:55:40 -0600

    change file name of central file from vault_util.py to vault.py; ensure that this is updated in the __init__ import and that no other references are made. Improve the dunder init file. Add to README.

--- src/dworshak-access/__init__.py
+++ src/dworshak-access/__init__.py
@@ -1,6 +1,7 @@
-from dworshak_accesss.vault_util import get_secret(), check_vault()
+from dworshak_accesss.vault import get_secret, check_vault
 
 __all__ = [
     "get_secret",
     "check_vault"
 ]
+
--- src/dworshak-access/vault.py
+++ src/dworshak-access/vault.py
@@ -1,27 +1,4 @@
-import sqlite3
-from pathlib import Path
-from cryptography.fernet import Fernet
-
-def get_secret_(service: str, item: str):
-    root = Path.home() / ".dworshak"
-    
-    # 1. Get the Key
-    key = (root / ".key").read_bytes()
-    f = Fernet(key)
-    
-    # 2. Query the DB
-    with sqlite3.connect(root / "vault.db") as conn:
-        res = conn.execute(
-            "SELECT secret FROM credentials WHERE service = ? AND item = ?", 
-            (service, item)
-        ).fetchone()
-    
-    # 3. Decrypt
-    return f.decrypt(res[0]).decode() if res else None
-
-
-# ---
-
+# src/dworshak-access/vault.py
 import sqlite3
 import os
 from pathlib import Path
