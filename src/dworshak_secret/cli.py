9191f59a440fc26e8197682b48462b0499a7d018
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-02-28 12:43:42 -0600

    improve cli.py description

--- src/dworshak_secret/cli.py
+++ src/dworshak_secret/cli.py
@@ -19,7 +19,7 @@ os.environ["TERM"] = "xterm-256color"
 
 app = typer.Typer(
     name="dworshak-secret",
-    help=f"Secure credential mamagement and orchestration. (v{__version__})",
+    help =f"Store and retrieve plaintext two-key credential values to encrypted database file. (v{__version__})",
     add_completion=False,
     invoke_without_command=True,
     no_args_is_help=True,
