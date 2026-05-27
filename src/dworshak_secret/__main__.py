aed1bfbac868ae401eff63070aa3a505a79ae2fc
George Clayton Bennett <george.bennett@memphistn.gov>
2026-05-27 14:55:11 -0500

    inckude click as extra dep in typer group, fix main to fail only if typer is not available

--- src/dworshak_secret/__main__.py
+++ src/dworshak_secret/__main__.py
@@ -2,17 +2,22 @@
 Entry point for dworshak-secret. 
 Favors the Rich/Typer CLI but falls back to a zero-dependency stdlib version.
 """
+TYPERSUCCESS=None
 try:
+    import typer
+    TYPERSUCCESS=True
+except (ImportError, ModuleNotFoundError):
+    # Fallback to the 'lifeboat' CLI
+    #from .cli_stdlib import main as run
+    TYPERSUCCESS=FALSE
+
+if TYPERSUCCESS:
     # Attempt to use the feature-rich CLI
     from .cli import app
     def run():
         app()
-except (ImportError, ModuleNotFoundError):
-    # Fallback to the 'lifeboat' CLI
-    #from .cli_stdlib import main as run
+else:
     import sys
-    #print("Please install this package with the 'typer' extra to utilize the CLI.", file=sys.stderr)
-    # Fallback when dependencies are missing
     def run():
         print(
             "Please install this package with the 'typer' extra to utilize the CLI.", 
