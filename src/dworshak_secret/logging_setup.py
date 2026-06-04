a652f2540f4ae8e7f3e00ab045c89379321d6459
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-06-04 00:42:52 -0500

    propogate new logging gospel

--- src/dworshak_secret/logging_setup.py
+++ src/dworshak_secret/logging_setup.py
@@ -1,8 +1,32 @@
-# src/dworshak_prompt/logging_setup.py
+# src/dworshak_secret/logging_setup.py
+from __future__ import annotations
 import logging
 import sys
 import traceback
+from rich.logging import RichHandler
+from rich.console import Console
+console = Console(stderr=True)
 
+def configure_root_logging_for_application(debug: bool,verbose: bool):
+    INTENT="app"
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers[:]:
+        root_logger.removeHandler(handler)
+
+    if verbose:
+        level = logging.INFO
+    elif debug:
+        level = logging.DEBUG
+    else:
+        level = logging.WARNING
+
+    root_logger.setLevel(level)
+    handler = RichHandler(console=console, show_time=False, show_path=debug,log_time_format="[%H:%M:%S]")
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    root_logger.addHandler(handler)
+    root_logger.debug(f"Debug logging enabled for {INTENT}.")
+    root_logger.info(f"Verbose logging enabled for {INTENT}.")
+    
 def setup_logging(verbose: bool = False, debug: bool = False, initial: bool=False):
     """
     Defunct.
